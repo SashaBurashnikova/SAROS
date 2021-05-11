aeb1f390414e9e29d6e6015e96409b06e9f7d574
SashaBurashnikova <22665911+SashaBurashnikova@users.noreply.github.com>
2021-05-12 00:29:40 +0300

    Update saros_convergence.py

--- saros_convergence.py
+++ saros_convergence.py
@@ -251,7 +251,7 @@ losses.append(train_loss[0])
 times.append(0)
 start_time = time.time()
 
-for epoch in range(10000000000):
+for epoch in range(200):
     user_losses = []
     for user_triplets in all_users_triplets:
         group_loss = 0
